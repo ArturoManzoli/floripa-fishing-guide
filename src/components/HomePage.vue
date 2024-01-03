e700ae55694b00fc24838f51107bc61fa731724a
Arturo <arturo@offertech.com.br>
2024-01-03 10:21:38 -0300

    add main page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-container>
+    <div class="d-flex flex-column mt-10">
+      <v-img src="@/assets/logo.png" height="150px" />
+    </div>
+    <h2 class="text-center mt-5">Floripa Fishing Guide</h2>
+  </v-container>
+</template>
